18fd4940bc1c7d24bb19fdbd6372473d5125d311
Ashley Le <ashleyle@usf.edu>
2025-02-23 13:15:07 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -18,3 +18,23 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 
 console.log(emp1.getDetails()); //"Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
 console.log(emp1.calculateAnnualSalary()); //60000
+
+//Task 2: Creating a Manager Class (Inheritance & Method Overriding)
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }; //adding team size to manager's responsibilities
+    getDetails() {
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    }; //overriding to include team size
+    calculateBonus() {
+        return this.salary * 12 * .1
+    }; //returns 10% of manager's annual salary
+
+};
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+
+console.log(mgr1.getDetails()); //Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5
+console.log(mgr1.calculateBonus()); //9600
