b82b970b6803b94cb7bcb35e2c4580d4ca7a8c78
Ashley Le <ashleyle@usf.edu>
2025-02-23 13:46:09 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -38,3 +38,24 @@ const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
 
 console.log(mgr1.getDetails()); //Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5
 console.log(mgr1.calculateBonus()); //9600
+
+//Task 3: Creating a Company Class
+class Company {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }; 
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }; //adds an employee to the array
+    listEmployees() {
+        this.employees.forEach(employee => {console.log(employee.getDetails())});
+    }; //logs all employee details
+};
+
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000
+// Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5
